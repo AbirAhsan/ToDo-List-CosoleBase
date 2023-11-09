cb39547a0cd9c9ec934301f2baa1ace37003b72c
Abir Ahsan <abirahsan122@gmail.com>
2023-11-09 22:16:38 +0600

    add mark as done function

--- task.go
+++ task.go
@@ -1,9 +1,19 @@
 package main
 
+import "fmt"
+
 type Task struct {
 	ID   int
 	Name string
 	Done bool
+}
+
+func (t *Task) MarkAsDone() {
+	if t.Done {
+		fmt.Println("Task Already Done")
+	}
+
+	t.Done = true
 }
 
 func NewTask(taskId int, taskName string) Task {
