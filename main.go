5a865bccc92a9836e1fce76b9f2406171815ca01
Abir Ahsan <abirahsan122@gmail.com>
2023-11-09 22:08:51 +0600

    view option and add condition for exit

--- main.go
+++ main.go
@@ -8,9 +8,32 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("===============TODO LIST===============")
+	for {
+		fmt.Println("===============TODO LIST===============")
+		fmt.Println("1. Add Task")
+		fmt.Println("2. View Task")
+		fmt.Println("3. Mark Task as Done")
+		fmt.Println("4. Exit")
+		fmt.Println("=======================================")
+		fmt.Println("Enter Your choice: ")
 
-	scanner.Scan()
-	str := scanner.Text()
-	fmt.Println("Scaned Text", str)
+		scanner.Scan()
+		choice := scanner.Text()
+		fmt.Println("Scaned Text", choice)
+
+		switch choice {
+		case "1":
+			fmt.Println("Add Task")
+		case "2":
+			fmt.Println("View Task")
+		case "3":
+			fmt.Println("Mark Task as Done")
+		case "4":
+			fmt.Println("Exit Task")
+			return
+		default:
+			fmt.Println("Invalid Choice")
+		}
+
+	}
 }
